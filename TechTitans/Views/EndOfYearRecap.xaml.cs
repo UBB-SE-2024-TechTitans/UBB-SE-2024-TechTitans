d6fb9bf56fc89d04178b506318da20ba063b18e6
Daniel Cujba <daniel.cujba.54@gmail.com>
2024-04-09 15:20:04 +0300

    End of Year Recap FE almost done

--- TechTitans/Views/EndOfYearRecap.xaml.cs
+++ TechTitans/Views/EndOfYearRecap.xaml.cs
@@ -1,11 +1,14 @@
 using TechTitans.Enums;
 using TechTitans.Models;
 using TechTitans.ViewModels;
+using TechTitans.Views.Components.EndOfYearRecap;
 
 namespace TechTitans.Views;
 
 public partial class EndOfYearRecap : ContentPage
 {
+    private int _pageIndex = 0;
+    private List<ProgressBar> _progressBar;
 	public EndOfYearRecap()
 	{
 		var mockSongs = new List<SongBasicInfo>()
@@ -80,8 +83,8 @@ public partial class EndOfYearRecap : ContentPage
         var viewModel = new EndOfYearRecapViewModel()
         {
             Top5MostListenedSongs = mockSongs,
-            MostPlayedSongPercentile = new Tuple<SongBasicInfo, decimal>(mockSongs.FirstOrDefault(), 0.1m),
-            MostPlayedArtistPercentile = new Tuple<string, decimal>("BDLP", 0.01m),
+            MostPlayedSongPercentile = new Tuple<SongBasicInfo, float>(mockSongs.FirstOrDefault(), 0.1f),
+            MostPlayedArtistPercentile = new Tuple<string, float>("BDLP", 0.01f),
             MinutesListened = 9000,
             Top5Genres = ["Manele", "Trap", "Rock", "Rap", "Pop"],
             NewGenresDiscovered = ["Jazz", "Populara", "Clasical", "R&B", "Country"],
@@ -89,7 +92,65 @@ public partial class EndOfYearRecap : ContentPage
         };
 
         BindingContext = viewModel;
+        InitializeComponent();
+        _progressBar = [ProgressBar1, ProgressBar2, ProgressBar3, ProgressBar4, ProgressBar5, ProgressBar6, ProgressBar7];
+        ChangeScreens();
+    }
 
-		InitializeComponent();
-	}
+    private async void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e) {
+        await _progressBar[_pageIndex - 1].ProgressTo(1,0,Easing.Linear);
+    }
+
+    private async void ChangeScreens()
+    {
+        _pageIndex++;
+        var localIndex = _pageIndex;
+        switch (_pageIndex)
+        {
+            case 1:
+                MainContentWindow.Content = new FirstScreen();
+                break;
+            case 2:
+                MainContentWindow.Content = new MinutesListenedScreen() 
+                { 
+                    BindingContext = BindingContext
+                };
+                break;
+            case 3:
+                MainContentWindow.Content = new Top5SongsScreen()
+                {
+                    BindingContext = BindingContext
+                };
+                break;
+            case 4:
+                MainContentWindow.Content = new MostPlayedSongScreen()
+                {
+                    BindingContext = BindingContext
+                };
+                break;
+            case 5:
+                MainContentWindow.Content = new MostPlayedArtistScreen()
+                {
+                    BindingContext = BindingContext
+                };
+                break;
+            case 6:
+                MainContentWindow.Content = new MostListenedGenresScreen()
+                {
+                    BindingContext = BindingContext
+                };
+                break;
+            case 7:
+                MainContentWindow.Content = new NewGenresScreen()
+                {
+                    BindingContext = BindingContext
+                };
+                break;
+            default:
+                Application.Current.MainPage = new NavigationPage(new MainPage());
+                return;
+        }
+        await _progressBar[_pageIndex - 1].ProgressTo(1, 5000, Easing.Linear);
+        ChangeScreens();
+    }
 }
--- TechTitans/Views/MainPage.xaml.cs
+++ TechTitans/Views/MainPage.xaml.cs
@@ -30,10 +30,10 @@ public partial class MainPage : ContentPage
         // TestId.Text = dbDataDemographic.UserId.ToString();
         // TestName.Text = dbDataDemographic.Name;
 
-        RepositoryAuthorDetails = new TestAuthorDetails();
+      /*  RepositoryAuthorDetails = new TestAuthorDetails();
         var dbDataAuthorDetails = RepositoryAuthorDetails.TestMethod();
         TestId.Text = dbDataAuthorDetails.ArtistId.ToString();
-        TestName.Text = dbDataAuthorDetails.Name;
+        TestName.Text = dbDataAuthorDetails.Name;*/
 
         // RepositorySongBasicDetails = new TestSongBasicDetails();
         // var dbDataSongBasicDetails = RepositorySongBasicDetails.TestMethod();
