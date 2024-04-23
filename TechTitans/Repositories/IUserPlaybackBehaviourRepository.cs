b025722b0165d87610b0b0f8e85ca9765e60af2f
OCTAVIAN-MIHAIL GHEORGHE <octavian.gheorghe@stud.ubbcluj.ro>
2024-04-23 17:17:11 +0300

    fixed errors

--- TechTitans/Repositories/IUserPlaybackBehaviourRepository.cs
+++ TechTitans/Repositories/IUserPlaybackBehaviourRepository.cs
@@ -9,7 +9,6 @@ namespace TechTitans.Repositories
 {
     internal interface IUserPlaybackBehaviourRepository
     {
-
         UserPlaybackBehaviour GetUserPlaybackBehaviour(int userId, int? songId = null, DateTime? timestamp = null);
         List<UserPlaybackBehaviour> GetUserPlaybackBehaviour(int userId);
     }
